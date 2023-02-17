a00543bbf47e81173c7977169124ec4ba16fda31
tacedoc <2170147754@qq.com>
2023-02-17 14:36:02 +0800

    feat: 优化Test2

--- src/main/java/com/sc/test/git/branch/Test2.java
+++ src/main/java/com/sc/test/git/branch/Test2.java
@@ -7,4 +7,7 @@ package com.sc.test.git.branch;
  * @date: 2023/2/17 14:27
  */
 public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("模拟dev分支完成新功能");
+    }
 }
