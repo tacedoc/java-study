9e49b7296ff0aa5ed646fa78bb15f6657b7dec14
tacedoc <2170147754@qq.com>
2023-02-17 14:32:27 +0800

    fix: 修复master分支bug

---
+++ src/main/java/com/sc/test/git/branch/Test5.java
@@ -0,0 +1,13 @@
+package com.sc.test.git.branch;
+
+/**
+ * @author: sc
+ * @version: 1.0
+ * @description:
+ * @date: 2023/2/17 14:31
+ */
+public class Test5 {
+    public static void main(String[] args) {
+        System.out.println("模拟已修复master分支bug");
+    }
+}
